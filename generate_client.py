bc07a90cf226db31d182f422a52e2138f992abfd
JenkinsCIRelease <austin.dickey@uptake.com>
2020-02-20 12:30:36 -0600

    Final bugfix; now it works

--- generate_client.py
+++ generate_client.py
@@ -3,12 +3,18 @@ import json
 import fitbit
 
 
+def load_creds():
+    with open("creds.json", "r") as f:
+        return json.load(f)
+
+
 def creds_expiry_callback(new_creds: dict):
     print("Gotta regenerate and save the creds again")
+    old_creds = load_creds()
 
     new_creds_to_save = {
-        "client_id": creds["client_id"],
-        "client_secret": creds["client_secret"],
+        "client_id": old_creds["client_id"],
+        "client_secret": old_creds["client_secret"],
         "access_token": new_creds["access_token"],
         "refresh_token": new_creds["refresh_token"],
         "expires_at": new_creds["expires_at"],
@@ -20,11 +26,8 @@ def creds_expiry_callback(new_creds: dict):
 
 
 def generate_client():
-    with open("creds.json", "r") as f:
-        creds = json.load(f)
-
     return fitbit.Fitbit(
-        **creds,
+        **load_creds(),
         refresh_cb=creds_expiry_callback,
         redirect_uri="http://127.0.0.1:8080/",
     )
